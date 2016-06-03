954cb9b8b85d8b419587c4b347decb4b03973dc4
David E <me@davidjeddy.com>
2016-06-03 12:53:30 +0900

    INIT commit

---
+++ src/Module.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace davidjeddy\modules\resutoran;
+
+/**
+ * resutoran module definition class
+ */
+class Module extends \yii\base\Module
+{
+    /**
+     * @inheritdoc
+     */
+    public $controllerNamespace = 'davidjeddy\modules\resutoran\common\controllers';
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
