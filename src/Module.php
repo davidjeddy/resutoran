51dd7b7845c69fae6e552f5c16a73d732942b1f7
David J Eddy <me@davidjeddy.com>
2016-07-21 19:06:49 -0400

    FIXed defaultController property in modules Module define class UPDATED formatting of `composer.json`

--- src/Module.php
+++ src/Module.php
@@ -10,7 +10,7 @@ class Module extends \yii\base\Module
     /**
      * @inheritdoc
      */
-    public $controllerNamespace = 'davidjeddy\modules\resutoran\common\controllers';
+    public $controllerNamespace = 'resutoran\common\controllers';
 
     /**
      * @inheritdoc
