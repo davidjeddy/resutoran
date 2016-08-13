a1e3f4000d769c170853a58ce58ac5e7b0a34598
David J Eddy <me@davidjeddy.com>
2016-08-13 16:26:00 -0400

    FIXED model reference in ResuFranchiseCNTL

--- src/backend/controllers/ResuFranchiseController.php
+++ src/backend/controllers/ResuFranchiseController.php
@@ -3,12 +3,12 @@
 namespace resutoran\backend\controllers;
 
 /**
- * ResuFranchiseController implements the CRUD actions for ResuFranchisemodel.
+ * ResuFranchiseController implements the CRUD actions for ResuFranchise.
  */
 class ResuFranchiseController extends \resutoran\backend\controllers\BaseController
 {
     /**
      * @var string
      */
-    protected $model = '\resutoran\common\models\ResuFranchis';
+    protected $model = '\resutoran\common\models\ResuFranchise';
 }
