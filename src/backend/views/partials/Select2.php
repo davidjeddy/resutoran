cb5496bcf24ae9d087764c168c54525db94fcde0
David J Eddy <me@davidjeddy.com>
2016-09-18 18:30:39 -0400

    DISABLED chosen.js required ability, see notes for source issue

--- src/backend/views/partials/Select2.php
+++ src/backend/views/partials/Select2.php
@@ -9,14 +9,26 @@ use yii\helpers\Inflector;
 $classPath= '\resutoran\common\models\Resu' . $options['label'];
 $modelClass = new $classPath();
 
+// Because chosen.js + chrome has an issue with highlighting required fields when the given field is `visibility:hidden`
+// ... only include the required attrib. on fields that require it, and do not include required=false on others.
+// ... the chosen.js client side validate pre-submit is disabled
+// @source https://github.com/harvesthq/chosen/issues/515
+$required = [];
+if (isset($options['required']) && $options['required'] === true) {
+    //$required = ['required' => true];
+}
+
 echo $options['form']->field($options['model'], 'resu_' . Inflector::underscore( $options['label'] ) . '_id')->widget(Select2::className(), [
     'data'      => ArrayHelper::map($modelClass::find()->all(), 'id', 'value'),
-    'options'   => [
-        'class'       => 'form-control',
-        'placeholder' => 'Choose Franchise...',
-        'multiple'    => (!empty($options['multiple']) ? $options['multiple'] : false),
-        'required'    => (!empty($options['required']) ? $options['required'] : false),
-    ],
+    'options'   => array_merge(
+        [
+            'class'       => 'form-control',
+            'placeholder' => 'Choose Franchise...',
+            'multiple'    => (!empty($options['multiple']) ? $options['multiple'] : false),
+
+        ],
+        $required
+    ),
     'pluginOptions' => [
         'allowClear' => (!empty($options['clear']) ? $options['clear'] : false),
     ],
