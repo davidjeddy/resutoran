87f7f69a94b8fe241ebc6966e7ef5eb5570d272f
David J Eddy <me@davidjeddy.com>
2017-01-29 19:39:11 -0500

    ADDED validators to ResuHourValue MDL

--- src/common/models/ResuHourValue.php
+++ src/common/models/ResuHourValue.php
@@ -37,6 +37,10 @@ class ResuHourValue extends \resutoran\common\models\ResuBase
             [['value'], 'required'],
             [['value'], 'string'],
             [['created_at', 'created_by', 'updated_at', 'updated_by', 'deleted_at'], 'integer'],
+
+            // custom validator
+            ['value', 'hasTwoTimes'],
+            ['value', 'validRange']
         ];
     }
 
@@ -46,8 +50,8 @@ class ResuHourValue extends \resutoran\common\models\ResuBase
     public function attributeLabels()
     {
         return [
-            'id' => Yii::t('resutoran', 'ID'),
-            'value' => Yii::t('resutoran', 'Value'),
+            'id'         => Yii::t('resutoran', 'ID'),
+            'value'      => Yii::t('resutoran', 'Value'),
             'created_at' => Yii::t('resutoran', 'Created At'),
             'created_by' => Yii::t('resutoran', 'Created By'),
             'updated_at' => Yii::t('resutoran', 'Updated At'),
@@ -66,10 +70,63 @@ class ResuHourValue extends \resutoran\common\models\ResuBase
 
     /**
      * @inheritdoc
-     * @return \common\models\query\ResuHourValueQuery the active query used by this AR class.
+     * @return \resutoran\common\models\query\ResuHourValueQuery the active query used by this AR class.
      */
     public static function find()
     {
         return new \resutoran\common\models\query\ResuHourValueQuery(get_called_class());
+    }
+
+    /**
+     * @param $attribute
+     * @param $params
+     *
+     * @return bool
+     */
+    public function hasTwoTimes($attribute, $params)
+    {
+        $timeArray = $this->convertValueToTime($this->value);
+
+        if (!empty($timeArray) && count($timeArray) != 2) {
+            $this->addError('value', 'Both an open and close time must be provided.');
+        }
+    }
+
+    /**
+     * @param $attribute
+     * @param $params
+     *
+     * @return bool
+     */
+    public function validRange($attribute, $params)
+    {
+        $timeArray = $this->convertValueToTime($this->value);
+
+        if (!empty($timeArray)
+            &&
+            (
+                [0] < 0
+                || $timeArray[0] > 2
+                || $timeArray[1] < 1
+                || $timeArray[1] > 24
+            )
+        ) {
+            $this->addError('value', 'Time is not valid.');
+        }
+    }
+
+    /**
+     * @param $param string
+     *
+     * @return array
+     */
+    private function convertValueToTime($param)
+    {
+        $timeArray = explode('-', $this->value);
+        foreach ($timeArray as $key => $value) {
+            $timeArray[$key] = (int)$value;
+        }
+
+        return $timeArray;
     }
 }
