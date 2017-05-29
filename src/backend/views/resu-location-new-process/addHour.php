b1490b8dfffa11d595fa92a7fd9ff97131fa3e3a
David J Eddy <me@davidjeddy.com>
2017-05-29 15:16:13 -0400

    Added null coales logic to addHour VW

--- src/backend/views/resu-location-new-process/addHour.php
+++ src/backend/views/resu-location-new-process/addHour.php
@@ -36,7 +36,7 @@ $this->params['breadcrumbs'][] = $this->title;
             echo Html::input(
                 'input',
                 'ResuLocationHour[' . $dayMDL->id . '][open]',
-                (string)$model[$key]->open,
+                ((string)$model[$key]->open ?? null),
                 [
                     'class'         => 'form-control',
                     'placeholder'   => '00:00 to 24:00 format',
@@ -48,7 +48,7 @@ $this->params['breadcrumbs'][] = $this->title;
             echo Html::input(
                 'text',
                 'ResuLocationHour[' . $dayMDL->id . '][close]',
-                (string)$model[$key]->close,
+                ((string)$model[$key]->close ?? null),
                 [
                     'class' => 'form-control',
                     'placeholder' => '00:00 to 24:00 format',
